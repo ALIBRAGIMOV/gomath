bf9c23648d495de30298054cc6a925fab96e6913
al1 <eminem05051@gmail.com>
2025-02-01 13:28:21 +0300

    main: init - add linked_list_cycle

--- Algorithms/leetcode_go/range_sum_2d/main.go
+++ Algorithms/leetcode_go/range_sum_2d/main.go
@@ -47,8 +47,8 @@ func (nm *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 // with prefix sum
 
 func main() {
-	matrix := [][]int{{
-		3, 0, 1, 4, 2},
+	matrix := [][]int{
+		{3, 0, 1, 4, 2},
 		{5, 6, 3, 2, 1},
 		{1, 2, 0, 1, 5},
 		{4, 1, 0, 1, 7},
